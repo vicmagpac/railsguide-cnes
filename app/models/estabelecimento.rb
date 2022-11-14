2c4c90bae7085f7a099aa7d525364f3969071510
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-11-14 15:21:48 -0300

    add model comment in the estabelecimento

--- app/models/estabelecimento.rb
+++ app/models/estabelecimento.rb
@@ -1,4 +1,6 @@
 class Estabelecimento < ApplicationRecord
+    has_many :comments
+
     validates :nome, presence: true
     validates :cnes, presence: true, length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_140132) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_181344) do
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "estabelecimento_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["estabelecimento_id"], name: "index_comments_on_estabelecimento_id"
+  end
+
   create_table "estabelecimentos", force: :cascade do |t|
     t.string "nome"
     t.string "cnes"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_140132) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "estabelecimentos"
 end
