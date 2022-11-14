47c28cd439d336acd3a14fb4d899b71470e3167a
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-11-14 15:39:10 -0300

    adding a router for comments and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "estabelecimentos#index"
   
-  resources :estabelecimentos
+  resources :estabelecimentos do
+    resources :comments
+  end
 end
