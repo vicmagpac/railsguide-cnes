1389b0c17a1befdd34bd7a79358d6d7c469cae3f
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-11-14 15:57:05 -0300

    adding form commenter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @estabelecimento = Estabelecimento.find(params[:estabelecimento_id])
+    $comment = @estabelecimento.comments.create(comment_params)
+
+    redirect_to estabelecimento_path(@estabelecimento)
+  end
+
+  private 
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
